b64e5789c2f44a3c43fa150b09bfecd0a7cbc3ec
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-02-04 12:52:07 +0300

    +js

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,7 @@ click_btn.addEventListener('click', function(event) {
 
 
 let titles = document.getElementsByTagName('h3');
+//let title = document.getElementsByClassName('hide');
 
 for (let i = 0; i < titles.length; i++) {
     titles[i].addEventListener('click', function(event) {
@@ -20,6 +21,20 @@ for (let i = 0; i < titles.length; i++) {
          return;
     } else {
        event.target.parentNode.nextElementSibling.classList.toggle('hide');
+       event.target.parentNode.nextElementSibling.classList.add('show');
+        }
+    })
+}
+
+let dat = document.getElementsByClassName('dat');
+for (let i = 0; i < dat.length; i++) {
+    dat[i].addEventListener('click', function(event) {
+        
+    if(event.target.nextElementSibling.tagName !== 'UL') {
+         return;
+    } else {
+       event.target.nextElementSibling.classList.toggle('hide');
+       event.target.nextElementSibling.classList.add('show_2');
         }
     })
 }
